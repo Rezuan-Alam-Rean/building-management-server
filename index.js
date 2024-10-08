997dd537f1653cd0c1ec18193e51843b0ce483a1
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2024-10-08 23:06:14 +0600

    comment logout

--- index.js
+++ index.js
@@ -201,21 +201,21 @@ async function run() {
     })
 
 
-    Logout
-    app.get('/logout', async (req, res) => {
-      try {
-        res
-          .clearCookie('token', {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-          })
-          .send({ success: true })
-        console.log('Logout successful')
-      } catch (err) {
-        res.status(500).send(err)
-      }
-    })
+    // Logout
+    // app.get('/logout', async (req, res) => {
+    //   try {
+    //     res
+    //       .clearCookie('token', {
+    //         maxAge: 0,
+    //         secure: process.env.NODE_ENV === 'production',
+    //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+    //       })
+    //       .send({ success: true })
+    //     console.log('Logout successful')
+    //   } catch (err) {
+    //     res.status(500).send(err)
+    //   }
+    // })
 
     // Get all users
     app.get('/users',  async (req, res) => {
